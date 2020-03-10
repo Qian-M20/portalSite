83b65d1a3bec92920ce0b984aacfda5a68b8329e
Qian <qian.ma3@dcmail.ca>
2020-03-10 11:34:00 -0400

    change the slider

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 window.onload = function(){
 
-
-$(document).ready(function(){
-    $('.slider').slick({
-        dots:true,
-        infinite:true,
-        fade:true,
-        cssEase:'linear',
-        autoplay:true,
-        autoplaySpeed:5000,
-        arrows:false,
-    });
-});
-
 $("body, html").animate({scrollTop: 0}, 300, "easeInSine");
 // changing navigation style on scroll
 const header = document.querySelector('header');
@@ -114,13 +101,30 @@ learnMore.forEach((btn) => {
     });
 })
 
+// create the clicking function on carousel indicators  
+
+$('.carousel-indicators').on("click",function(){
+
+    $holder = $( ".carousel-item.active" );
+    $holder.next( "div" ).addClass("active");  
+    if($holder.hasClass('slider3'))
+    {
+        $holder.removeClass("active");
+        $(".carousel-item.slider1").addClass("active");
+    }
+    $holder.removeClass("active");
+
+});
+
 // when click on the exit button, close the wrapper 
 
 exitBtn.forEach((btn) => {
     btn.addEventListener('click', function(){
+        // console.log('closing button');
         $(`.wrapper.${theID}`).hide(); 
         $(`.proReviewContent`).removeClass('active');       
     });
 })
+
 
 }
